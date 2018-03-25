267dea5edd2be68fbbf175a13d7d8fec14c58357
iSun <sslvjun@gmail.com>
2018-03-25 21:34:02 +0800

    Variable declaration test.

---
+++ chapter2/variable_declaration.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var v_name1 int32
+	v_name1 = 3456
+	fmt.Println("v_name1: ", v_name1)
+
+    var v_name2 = 3456
+    fmt.Println("v_name2:", v_name2)
+
+     var a1 int = 10
+     var b1 = 10
+     c1 := 10
+     fmt.Println("a1:", a1, "b1:", b1, "c1:", c1)
+
+     var a2 = "Course of rookie"
+     var b2 string = "runoob.com"
+     var c2 bool
+     fmt.Println("a2:", a2, "b2:", b2, "c2:", c2)
+}
